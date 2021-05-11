cd95196ef01cf9e6706c5594779b4673237ce733
Yimmy Lopez <yimmy.lopez@gmail.com>
2021-05-11 14:15:08 -0400

    inicio

--- src/main/java/com/example/springproyect/SpringproyectApplication.java
+++ src/main/java/com/example/springproyect/SpringproyectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringproyectApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(SpringproyectApplication.class, args);
     }
