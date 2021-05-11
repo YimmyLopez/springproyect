17324ad961da0acf8708a73c16be31d7af0b4766
Yimmy Lopez <yimmy.lopez@gmail.com>
2021-05-11 14:14:05 -0400

    inicio

---
+++ src/main/java/com/example/springproyect/SpringproyectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springproyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringproyectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringproyectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springproyect/SpringproyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringproyectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
